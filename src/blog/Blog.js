87b900542353ec8ac32465cffafafa57878e51ae
Fredy Somy <fredysomy@gmail.com>
2021-01-06 07:23:52 +0530

    Update Blog.js

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -9,14 +9,14 @@ export default class Blog extends Component{
  componentWillMount() {
     axios.get('https://dev.to/api/articles/me/published',{ headers :{ "api-key":"e7wq9EdnLxhWvMGzP75kLAYD"},})
     .then(res =>{
-        this.setState(res)
+        console.log(res)
     })
  }
   render(){
     return(
       <div>
       <Link to="/asd">Hiii</Link>
-      {this.state.blog[1]}
+      
       </div>
     )
   }
